0ecb1b4db55d1dab782c9d38dcb0d90042441891
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-06-30 23:36:00 -0400

    Restructured Project
    Using ASP .NET 4.8
    MVC + WebAPI

---
+++ EMS/Employee Management System/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee_Management_System.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+
+        public string Name { get; set; }
+
+        public Employee(){}
+    }
+}
