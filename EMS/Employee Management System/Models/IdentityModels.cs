cdc02dd978bb367fa896b1b61ed424b4c2a55c21
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-07-01 23:59:33 -0400

    Added Employee Context

--- EMS/Employee Management System/Models/IdentityModels.cs
+++ EMS/Employee Management System/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -29,5 +30,8 @@ namespace Employee_Management_System.Models
         {
             return new ApplicationDbContext();
         }
+
+        //Required context for Employee
+        public DbSet<Employee> Employees { get; set; }
     }
 }
