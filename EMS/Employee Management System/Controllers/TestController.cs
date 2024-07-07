1bfbbf1a690a76b4feaeeac9d6236c377db3829c
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-07-07 18:41:20 -0400

    Experimental Controller

---
+++ EMS/Employee Management System/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee_Management_System.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test2
+        public string Index()
+        {
+            return "this is my <b> default  </b> action";
+        }
+
+        public string Welcome() {
+            return "a";
+        }
+    }
+}
