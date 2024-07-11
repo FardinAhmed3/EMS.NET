e237ea66b192852dae332e2267f19b704bb68480
Fardin <98803821+FardinAhmed3@users.noreply.github.com>
2024-07-11 19:29:23 -0400

    Typo

--- EMS/Employee Management System/Controllers/TestController.cs
+++ EMS/Employee Management System/Controllers/TestController.cs
@@ -8,7 +8,7 @@ namespace Employee_Management_System.Controllers
 {
     public class TestController : Controller
     {
-        // GET: Test2
+        // GET: Test
         public string Index()
         {
             return "this is my <b> default  </b> action";
