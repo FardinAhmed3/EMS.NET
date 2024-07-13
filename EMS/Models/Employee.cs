188fe73f410e9cf2d194a579cca9d561b9db0f25
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-07-13 01:36:37 -0400

    Added Employee model & data handler

---
+++ EMS/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EMS.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Position { get; set; }
+
+        [Required]
+        public Department Department { get; set; }
+
+        [StringLength(100)]
+        public string City { get; set; }
+    }
+
+    public enum Department
+    {
+        Technology,
+        Finance,
+        Operations
+    }
+}
